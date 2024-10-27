534e4723e8ef8f485545278787028a07392c688f
plougue <guillot.pl@gmail.com>
2024-10-27 18:02:23 +0000

    Update is_n_choose_k_odd.cpp for typo

--- is_n_choose_k_odd.cpp
+++ is_n_choose_k_odd.cpp
@@ -6,7 +6,7 @@ bool is_n_choose_k_odd(int n, int k) {
 This is just an application of Lucas Theorem 
 https://en.wikipedia.org/wiki/Lucas%27s_theorem
 
-Or even or the most involed Kummer theorem
+Or even of the most involed Kummer theorem
 https://en.wikipedia.org/wiki/Kummer%27s_theorem
 
 */
